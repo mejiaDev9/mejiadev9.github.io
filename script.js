1220b488a91194e547d7e265bae4acf6d9cdbbc1
Jhon Mark Mejia <mejiajhonmark.c@gmail.com>
2024-07-14 21:12:10 +0800

    .

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const navList = document.querySelector('.navbar ul');
-const navLinks = document.querySelectorAll('.navbar a');
-const hamburger = document.querySelector('.navbar__toggler');
+        const navLinks = document.querySelectorAll('.navbar a');
+        const hamburger = document.querySelector('.navbar__toggler');
 
-hamburger.addEventListener("click", navbarTogglerClick);
+        hamburger.addEventListener("click", navbarTogglerClick);
 
-function navbarTogglerClick() {
-    hamburger.classList.toggle("open-navbar__toggler");
-    navList.classList.toggle("open");
-}
+        function navbarTogglerClick() {
+            hamburger.classList.toggle("open-navbar__toggler");
+            navList.classList.toggle("open");
+        }
 
-navLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
+        navLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
 
-function navbarLinkClick() {
-    if (navList.classList.contains("open")) {
-        hamburger.click();
-    }
-}
+        function navbarLinkClick() {
+            if (navList.classList.contains("open")) {
+                hamburger.click();
+            }
+        } 
+ 
